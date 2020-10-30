63e5a6380afadb294d297beb0c89dfc89fb5a92d
Jakub Boniecki <ExtriPL@gmail.com>
2020-10-30 21:01:10 +0100

    Dodanie animacji rzucania kostką

---
+++ Assets/Scripts/Gameplay/Board/Fields/Specials/Chance/Actions/WithUserAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class WithUserAction : ActionCard
+{
+    private ActionCard insideAction;
+
+    public WithUserAction(ActionCard insideAction)
+    {
+        this.insideAction = insideAction;
+    }
+
+    public override void Call(Player caller)
+    {
+        throw new NotImplementedException();
+    }
+}
