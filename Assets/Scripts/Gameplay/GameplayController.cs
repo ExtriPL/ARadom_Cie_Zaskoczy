10867447e4d15fc343869dfefbd8123c8950f9d6
Jakub Boniecki <ExtriPL@gmail.com>
2020-11-02 13:19:53 +0100

    Naprawa

--- Assets/Scripts/Gameplay/GameplayController.cs
+++ Assets/Scripts/Gameplay/GameplayController.cs
@@ -334,7 +334,7 @@ public class GameplayController : MonoBehaviour, IEventSubscribable
             if (lang.IsPacked(word))
                 unpackedMessage += lang.GetWord(lang.UnpackKey(word));
             else
-                unpackedMessage += word;td
+                unpackedMessage += word;
         }
 
         IconPopup messagePopup = new IconPopup(iconType, unpackedMessage);
